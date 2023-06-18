781a969c22f15422ccb9bb7a197dd39fc8c05eaf
aulth <mohdusman.you@gmail.com>
2023-06-18 07:55:53 +0530

    debugging 6

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../../../components/blog/Navbar'
-import ArticleComponent from '../../../components/blog/article/ArticleComponent'
 
-const page = ({ data }) => {
+const page = ({ data, slug }) => {
     return (
         <>
             <Navbar />
             {data && JSON.stringify(data)}
+            {slug}
         </>
     )
 }
@@ -15,25 +15,26 @@ export default page
 
 export async function getServerSideProps(context) {
     const {slug} = context.params
-    console.log(slug)
-    const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blog/fetchone`, {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
-        body: JSON.stringify({ link: 'the-power-of-exercise-physical-and-mental-benefits' }),
-    });
+    // const response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/blog/fetchone`, {
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'application/json',
+    //     },
+    //     body: JSON.stringify({ link: 'the-power-of-exercise-physical-and-mental-benefits' }),
+    // });
 
-    let data = await response.json();
-    console.log(data)
-    if (data.success) {
-        data = data.article;
-    } else {
-        data = null;
-    }
+    // let data = await response.json();
+    // console.log(data)
+    // if (data.success) {
+    //     data = data.article;
+    // } else {
+    //     data = null;
+    // }
+    const data = ""
     return {
         props: {
             data: data,
+            slug:slug
         },
     }
 }
