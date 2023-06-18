85872a172c23825661ed2a24075864f4fa2c55fd
aulth <mohdusman.you@gmail.com>
2023-06-18 10:15:42 +0530

    reverted

--- components/blog/article/Body.jsx
+++ components/blog/article/Body.jsx
@@ -1,33 +1,9 @@
 import Link from 'next/link'
-import React, { useEffect , useState} from 'react'
+import React, { useEffect } from 'react'
 import { AiOutlineShareAlt } from 'react-icons/ai'
 import toast, { Toaster } from 'react-hot-toast';
 import { RiPencilFill } from 'react-icons/ri'
-import {
-    FacebookShareButton,
-    FacebookIcon,
-    TwitterShareButton,
-    TwitterIcon,
-    LinkedInShareButton,
-    LinkedinIcon,
-    RedditShareButton,
-    RedditIcon,
-    PinterestShareButton,
-    PinterestIcon,
-    TelegramShareButton,
-    TelegramIcon,
-    WhatsappShareButton,
-    WhatsappIcon,
-    EmailShareButton,
-    EmailIcon
-} from 'next-share';
-import { LinkOutlined } from '@mui/icons-material';
 const Body = ({ data }) => {
-    const [currentUrl, setCurrentUrl] = useState('');
-
-    useEffect(() => {
-        setCurrentUrl(window.location.href);
-    }, []);
     const getFormattedDate = (str) => {
         const date = new Date(str);
         const month = date.toLocaleString('default', { month: 'long' });
@@ -106,48 +82,9 @@ const Body = ({ data }) => {
                 <h1 className="font-bold text-2xl mt-2 capitalize">{data.title}</h1>
                 <div className="w-full flex justify-between items-center text-sm mt-2">
                     <time class="text-gray-500 text-sm"><span class="mr-1">Published:</span> {getFormattedDate(data.createdAt)}</time>
-                    {/* <button onClick={sharePost} className='hover:text-cyan-500'><AiOutlineShareAlt /></button> */}
-                    <div className="flex md:mt-0 mt-2 justify-end gap-2">
-                        <FacebookShareButton
-                            url={currentUrl}
-                            quote={data.title}
-                            hashtag={'#' + data.category.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join('')}
-                        >
-                            <FacebookIcon size={24} round />
-                        </FacebookShareButton>
-                        <TwitterShareButton url={currentUrl} title={data.title}>
-                            <TwitterIcon size={24} round />
-                        </TwitterShareButton>
-                        <PinterestShareButton
-                            url={currentUrl}
-                            media={data.cover}
-                        >
-                            <PinterestIcon size={24} round />
-                        </PinterestShareButton>
-                        <RedditShareButton
-                            url={currentUrl}
-                            title={data.title}
-                        >
-                            <RedditIcon size={24} round />
-                        </RedditShareButton>
-                        <TelegramShareButton
-                            url={currentUrl}
-                            title={data.title}
-                        >
-                            <TelegramIcon size={24} round />
-                        </TelegramShareButton>
-                        <WhatsappShareButton
-                            url={currentUrl}
-                            title={data.title}
-                        >
-                            <WhatsappIcon size={24} round />
-                        </WhatsappShareButton>
-                        <button onClick={() => { navigator.clipboard.writeText(currentUrl); toast.success("Copied") }} className='bg-gray-200 p-0.5 rounded-full text-center aspect-square w-6'>
-                            <LinkOutlined className='text-sm rotate-45' />
-                        </button>
-                    </div>
+                    <button onClick={sharePost} className='hover:text-cyan-500'><AiOutlineShareAlt /></button>
                 </div>
-                <main className='mt-4 body article-body clear-none'>
+                <main  className='mt-4 body article-body clear-none'>
                 </main>
             </div>
         </>
