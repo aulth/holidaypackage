d5a130e986483c118ece855e0a9386326724d90c
aulth <mohdusman.you@gmail.com>
2023-06-18 08:34:49 +0530

    debugging 10

--- components/blog/article/ArticleComponent.jsx
+++ components/blog/article/ArticleComponent.jsx
@@ -23,7 +23,7 @@ const ArticleComponent = ({ data }) => {
         <div className="container m-auto flex gap-4 flex-col xl:flex-row xl:px-16 px-4 py-4">
             <div className="w-full flex flex-col gap-4 xl:w-[60%]">
                 <div className="w-full rounded-lg bg-white ">
-                    <Body data={data} />
+                    {/* <Body data={data} /> */}
                 </div>
                 <div className="w-full rounded-lg bg-white ">
                     <YouMightLike category={data.category} />
--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,39 +1,32 @@
 import React from 'react'
 import Navbar from '../../../components/blog/Navbar'
-import PostComment from '../../../components/blog/article/PostComment'
-import Comment from '../../../components/blog/article/Comment'
-import Popular from '../../../components/blog/home/Popular'
-import Subscribe from '../../../components/blog/home/Subscribe'
-import Body from '../../../components/blog/article/Body'
+import Head from 'next/head'
+import ArticleComponent from '../../../components/blog/article/ArticleComponent'
+import Footer from '../../../components/Footer'
 const Page = ({ data, slug }) => {
     return (
         <>
             <Navbar />
-            {data && JSON.stringify(data)}
-            <div className="container m-auto flex gap-4 flex-col xl:flex-row xl:px-16 px-4 py-4">
-                <div className="w-full flex flex-col gap-4 xl:w-[60%]">
-                    <div className="w-full rounded-lg bg-white ">
-                        <Body data={data} />
-                    </div>
-                    {/* <div className="w-full rounded-lg bg-white ">
-                        <YouMightLike category={data.category} />
-                    </div> */}
-                    <div className="w-full rounded-lg bg-white ">
-                        <Comment data={data.remarks} />
-                    </div>
-                    <div className="w-full rounded-lg bg-white ">
-                        <PostComment id={data._id} />
-                    </div>
+            {data && data.live && (
+                <>
+                    <Head>
+                        {/* Meta tags */}
+                        <title>{data.title.slice(0, 69)}</title>
+                        <meta name="title" content={data.title.slice(0, 69)} />
+                        <meta name="description" content={data.content.slice(0, 150).replace(/<[^>]+>/g, '')} />
+                    </Head>
+                    <ArticleComponent data={data} />
+                </>
+            )}
+            {!data && (
+                <div className="container m-auto md:px-12 px-4 py-4">
+                    This article is not available {slug}
                 </div>
-                <aside className="w-full flex flex-col gap-4 xl:w-[40%]">
-                    <div className="w-full rounded-lg bg-white">
-                        <Popular />
-                    </div>
-                    <div className="w-full rounded-lg bg-white">
-                        <Subscribe />
-                    </div>
-                </aside>
-            </div>
+            )}
+            {data && !data.live && (
+                <div className="container m-auto md:px-12 px-4 py-4">This article has been made private</div>
+            )}
+            <Footer />
         </>
     )
 }
