73547b844ef4da84dc3280ea8ba0fe747d3f15ab
aulth <mohdusman.you@gmail.com>
2023-06-18 10:19:05 +0530

    breadcrumb updated in blog

--- components/blog/article/Body.jsx
+++ components/blog/article/Body.jsx
@@ -70,13 +70,22 @@ const Body = ({ data }) => {
             <div className="w-full rounded-lg p-4">
                 <nav className="text-sm text-cyan-500">
                     <ol className='list-none p-0 inline-flex gap-1'>
-                        <li class="flex gap-1 items-center">
-                            <Link href={'/blog'} className='hover:text-cyan-600 hover:underline'>Home</Link>
+                        <li className="flex gap-1 items-center">
+                            <Link href={'/blog'} className='hover:text-red-600 hover:underline'>Home</Link>
                             &gt;
                         </li>
-                        <li class="flex items-center">
-                            <Link href={`/blog/category/${data.category}`} className='hover:text-cyan-600 hover:underline'>{data.category[0].toUpperCase() + data.category.slice(1).toLowerCase()}</Link>
-                        </li>
+                        {
+                            data.category.split(' ').map((item, index) => (
+                                <React.Fragment key={index}>
+                                    <li className="flex items-center">
+                                        <Link href={`/blog/category/${item.toLowerCase()}`} className="hover:text-red-600 hover:underline">
+                                            {item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()}
+                                        </Link>
+                                    </li>
+                                    {index < data.category.split(' ').length - 1 && '>'}
+                                </React.Fragment>
+                            ))
+                        }
                     </ol>
                 </nav>
                 <h1 className="font-bold text-2xl mt-2 capitalize">{data.title}</h1>
@@ -84,7 +93,7 @@ const Body = ({ data }) => {
                     <time class="text-gray-500 text-sm"><span class="mr-1">Published:</span> {getFormattedDate(data.createdAt)}</time>
                     <button onClick={sharePost} className='hover:text-cyan-500'><AiOutlineShareAlt /></button>
                 </div>
-                <main  className='mt-4 body article-body clear-none'>
+                <main className='mt-4 body article-body clear-none'>
                 </main>
             </div>
         </>
