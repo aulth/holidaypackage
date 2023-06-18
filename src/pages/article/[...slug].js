430b2c22e39493635a69d9da0faf66faec0d40f8
aulth <mohdusman.you@gmail.com>
2023-06-18 09:01:20 +0530

    debugging 13

--- src/pages/article/[...slug].js
+++ src/pages/article/[...slug].js
@@ -1,25 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../../components/Packages/Navbar'
-import Carousel from '../../../components/Visa/Carousel'
-import IconButton from '@mui/material/IconButton';
-import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import Itinerary from '../../../components/Packages/Itinerary';
-import InclusionsExclusions from '../../../components/Packages/InclusionsExclusions';
-import Flights from '../../../components/Packages/Flights';
-import Overview from '../../../components/Packages/Overview';
-import Pricing from '../../../components/Packages/Pricing';
-import Gallery from '../../../components/Packages/Gallery';
-import Date from '../../../components/Packages/DateComponent';
-import TermsAndConditions from '../../../components/Packages/TermsAndConditions';
-import BookNow from '../../../components/Packages/BookNow';
-import MenuOpenOutlinedIcon from '@mui/icons-material/MenuOpenOutlined';
-import NeedAssistance from '../../../components/Packages/NeedAssistance';
-import MoreVisa from '../../../components/Visa/MoreVisa';
-import Footer from '../../../components/Footer';
-import Documents from '../../../components/Visa/Documents';
+
 import NotFound from '../../../components/NotFound';
 import Booking from '../../../components/Visa/Book/Booking';
+import ArticleComponent from '../../../components/blog/article/ArticleComponent';
 const page = ({ data, link }) => {
     useEffect(() => {
         // incrementView();
@@ -37,10 +21,10 @@ const page = ({ data, link }) => {
         <>
             {
                 data &&
-                <div className="overflow-x-hidden   h-screen relative ">
+                <div className="overflow-x-hidden h-screen relative ">
                     <Navbar />
                     {/* Tour Details Menu  */}
-                    {JSON.stringify(data)}
+                    <ArticleComponent data={data} />
                 </div>
             }
             {
